441fb0b13e63a6f2cc5cc456b383c26a9377dae8
Chris Joo <chrisj1225@gmail.com>
2021-04-02 15:43:19 -0400

    Seeded DB, created index and show views

--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cat1 = Cat.create(birth_date: '2015/01/20', color: 'orange', name: 'Garfield', sex: 'M', description: 'A fat & lazy cat')
+
+cat2 = Cat.create(birth_date: '2014/07/04', color: 'white', name: 'Sassy', sex: 'F', description: 'A very sassy cat')
+
+cat3 = Cat.create(birth_date: '2013/12/25', color: 'grey', name: 'Christmas', sex: 'M', description: 'A very happy cat')
+
+
